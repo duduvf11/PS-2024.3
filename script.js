3028b7af1f61eb00284b2180ba2b193cca7457da
Eduardo Viegas Francisco <duduviegasfrancisco11@gmail.com>
2024-10-11 15:51:00 -0300

    grey view on toggle

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
+    let menuGreyView = document.querySelector('.grey-view')
+
     if(menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open')
+        menuGreyView.classList.remove('open')
     } else {
         menuMobile.classList.add('open')
+        menuGreyView.classList.add('open')
     }
 }
