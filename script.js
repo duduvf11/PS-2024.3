40e27fcb9f027b9b5f67866867f9bd7cc3cd4679
Eduardo Viegas Francisco <duduviegasfrancisco11@gmail.com>
2024-10-12 21:30:15 -0300

    carrossel done

--- script.js
+++ script.js
@@ -10,3 +10,36 @@ function menuShow() {
         menuGreyView.classList.add('open')
     }
 }
+
+const controls = document.querySelectorAll('.control');
+
+let currentItem = 0;
+const items = document.querySelectorAll('.card-item');
+const maxItems = items.length;
+
+controls.forEach(control =>{
+    control.addEventListener('click', () =>{
+        const isLeft = control.classList.contains("arrow-left")
+
+        if(isLeft) {
+            currentItem -= 1;
+        } else {
+            currentItem += 1;
+        }
+
+        if (currentItem >= maxItems){
+            currentItem = 0;
+        }
+
+        if (currentItem < 0){
+            currentItem = maxItems - 1;
+        }
+
+        items[currentItem].scrollIntoView({
+            inline: "center",
+            behavior: "smooth"
+        })
+        
+    })
+})
+
